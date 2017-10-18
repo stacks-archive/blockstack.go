0bb50cabad45d1d185b2dcc1ff96b3229f2a8f44
Jack Zampolin <jack.zampolin@gmail.com>
2017-10-17 22:49:13 -0700

    remove comments

--- cmd/blockstack/cmd/getinfo.go
+++ cmd/blockstack/cmd/getinfo.go
@@ -37,14 +37,4 @@ var getinfoCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(getinfoCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getinfoCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getinfoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
